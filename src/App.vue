03ff6793caeb4e651cbc5f395985c4ac82bdaaa9
Aarya Bhatia <aaryabhatia@Aaryas-MacBook-Air.local>
2020-08-09 12:30:23 +0530

    I DID IT AT LAST

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" placeholder="name" v-model="user" />
+
+    <div v-if="!joinedRoom">
+      <input type="text" placeholder="room id" v-model="roomName">
+      <button @click="joinRoom">Join Room</button>
+    </div>
+    <div v-else>
+      <button @click="leaveRoom">Leave Room</button>
+
+      <form @submit.prevent="sendMessage">
+        <input type="text" placeholder="type here..." v-model="content" />
+      </form>
+
+      <div v-for="(message, i) in messages" :key="i">{{ message.user }} -> {{ message.content }}</div>
+    </div>
+
+    <div>{{ message }}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      user: "",
+      joinedRoom: false,
+      roomName: "HomeRoom",
+      message: "",
+      content: "",
+      messages: [],
+    };
+  },
+  sockets: {
+    connect() {
+      console.log("socket connected");
+    },
+    USER_JOINED(user) {
+      this.message = user + " joined room " + this.roomName + '!';
+      this.joinedRoom = true;
+    },
+    USER_LEFT(user) {
+      this.message = user + " left room " + this.roomName + '!';
+      this.joinedRoom = false;
+    },
+    NEW_MESSAGE({ user, content }) {
+      this.message = user + " sent a message!"
+      this.messages.push({ user, content })
+    }
+  },
+  methods: {
+    joinRoom() {
+      this.$socket.emit("subscribe", {
+        room: this.roomName,
+        user: this.user,
+      });
+    },
+    leaveRoom() {
+      this.$socket.emit("unsubscribe", {
+        room: this.roomName,
+        user: this.user,
+      });
+    },
+    sendMessage() {
+      this.$socket.emit("new message", {
+        room: this.roomName,
+        user: this.user,
+        content: this.content,
+      });
+    },
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import VueSocketio from 'vue-socket.io'
+import socketio from 'socket.io-client'
 
 Vue.config.productionTip = false
+
+const SocketInstance = socketio.connect('http://localhost:3000/chats');
+
+Vue.use(new VueSocketio({
+  debug: true,
+  connection: SocketInstance,
+  store
+}));
 
 new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+export default SocketInstance;
